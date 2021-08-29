05d8e06f8b17fdb17f966e7211182164921ac359
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-08-29 16:12:45 +0700

    GGEZ

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CurrentPrice from './components/CurrentPrice';
+import HistoricalPrice from './components/HistoricalPrice';
+import Result from './components/Result';
+import AboutMe from './components/AboutMe';
 
 function App() {
+
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' exact>
+          <CurrentPrice/>
+        </Route>
 
-      {/* template for /current */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Current price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
-        <p> (Last updated) </p>
-      </div>
+        <Route path='/current'>
+          <CurrentPrice/>
+        </Route>
 
-      <br />
+        <Route path='/history/select'>
+          <HistoricalPrice />
+        </Route>
 
-      {/* template for /history/select */}
-      <div className='text-center space-y-3 space-x-3'>
-        <p className='text-2xl font-semibold'>Select historical range</p>
-        <span>From date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <span>To date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <br />
-        <button>Get data</button>
-      </div>
+        <Route path='/history/result'>
+          <Result />
+        </Route>
 
-      <br />
-
-      {/* template for /history/result */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Historical price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
-        <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
-        <ul>
-          <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
-        </ul>
-      </div>
-
-      <br />
-
-      {/* template for about me */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>About me</p>
-        <p className='text-xl'>Chayanin Suatap 610631100</p>
-      </div>
+        <Route path='/about'>
+          <AboutMe />
+        </Route>
+      </Switch>
 
     </Router>
   );
