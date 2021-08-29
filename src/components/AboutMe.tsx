c2cacabdaa73c14787a7ecb9714197aca00182d7
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-08-29 16:14:28 +0700

    GGEZ

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Phumiphat Srikrachang 630610750</p>
+        </div>
+    )
+}
+
+export default AboutMe
